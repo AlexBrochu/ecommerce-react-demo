ac57dbd2b54ab850772b4137d8baf8bc8d987f70
AlexBrochu <alexandre.brochu.1@ens.etsmtl.ca>
2021-04-04 20:57:59 -0400

    firebase store new users

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import ShopPage from "./pages/shop/shoppage.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 class App extends React.Component<{}, any> {
   constructor(props: any) {
@@ -20,8 +20,26 @@ class App extends React.Component<{}, any> {
   unsubscribeFromAuth = () => {};
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef?.onSnapshot(
+          (snapShot) => {
+            this.setState({
+              currentUser: {
+                id: snapShot.id,
+                ...snapShot.data(),
+              },
+            });
+          },
+          //setState is asynchronus
+          () => {
+            console.log(this.state);
+          }
+        );
+      }
+
+      this.setState({ currentUser: userAuth });
     });
   }
 
--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -7,7 +7,6 @@ import { auth } from "../../firebase/firebase.utils";
 import "./header.styles.scss";
 
 const Header = ({ currentUser }: any) => {
-  console.log(currentUser);
   return (
     <div className="header">
       <Link className="logo-container" to="/">
